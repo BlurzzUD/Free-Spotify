438f1bb2cf825bcb1f5138b9ffbf0497a8b33cb1
Just a DEV <bsnrpages@gmail.com>
2024-05-22 19:26:17 +0200

    Add files via upload

---
+++ Free-spotify-premium-main/Free-spotify-premium-main/SpotifyPremium/SpotifyPremium-main/PatchSpotify.bat
@@ -0,0 +1,17 @@
+@echo off
+COLOR 0a
+MODE con:cols=100 lines=20
+TITLE Cracked By BlurzzUD ez 
+taskkill /f /im Spotify.exe
+xcopy /s "%~dp0chrome_elf.dll" "%appdata%\Spotify" /y
+xcopy /s "%~dp0config.ini" "%appdata%\Spotify" /y
+icacls "%localappdata%\Spotify\Update" /reset /T
+del /s /q "%localappdata%\Spotify\Update"
+mkdir "%localappdata%\Spotify\Update"
+icacls "%localappdata%\Spotify\Update" /deny "%username%":W
+if not exist "%appdata%\Spotify\" mkdir "%appdata%\Spotify"
+del "%appdata%\Spotify\SpotifyMigrator.exe"
+del "%appdata%\Spotify\SpotifyStartupTask.exe"
+start https://discord.gg/xaQrFYyqAF
+pause
+exit
